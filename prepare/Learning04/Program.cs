515995299f764491c72e7236ef4231e14b3c0674
iheanyi11 <ejeagbaakuegbo@gmail.com>
2024-05-16 18:47:06 +0100

    Finished Learning04 activity

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+       Assignment assignment1 = new Assignment("Samuel Bennett", "Multiplication");
+       string summary = assignment1.GetSummary();
+       Console.WriteLine(summary);
+
+       MathAssignment assignment2 = new MathAssignment("Samuel Bennett", "Multiplication", "7.3", "8-19");
+       string summary2 = assignment2.GetSummary();
+       string assignmentNd2 = assignment2.GetHomeworkList();
+       Console.WriteLine(summary2);
+       Console.WriteLine(assignmentNd2);
+
+        WritingAssignment assignment3 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(assignment3.GetSummary());
+        Console.WriteLine(assignment3.GetWritingInformation());
     }
 }
