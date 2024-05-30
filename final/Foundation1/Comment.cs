ab874cc01d1b722074039016f14450f0121c5e16
iheanyi11 <ejeagbaakuegbo@gmail.com>
2024-05-30 20:58:30 +0100

    Foundation 1 program completed

---
+++ final/Foundation1/Comment.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Comment
+{
+    public string _commenter;
+    public string _text;
+
+    public Comment(string commenter, string text)
+    {
+        _commenter = commenter;
+        _text = text;
+    }
+}
--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        // Create 3 videos
+        List<Video> _videos = new List<Video>
+        {
+            new Video("Video 1", "Creator A", 120),
+            new Video("Video 2", "Creator B", 180),
+            new Video("Video 3", "Creator A", 240)
+        };
+
+        // Add comments to each video
+        _videos[0]._comments.Add(new Comment("User 1", "Great video!" ));
+        _videos[0]._comments.Add(new Comment("User 2", "I learned a lot from this."));
+        _videos[0]._comments.Add(new Comment("User 3", "Awesome content!"));
+
+        _videos[1]._comments.Add(new Comment("User 4", "Informative and engaging."));
+        _videos[1]._comments.Add(new Comment("User 5", "Loved the production quality."));
+        _videos[1]._comments.Add(new Comment("User 6", "Can't wait for more videos like this."));
+
+        _videos[2]._comments.Add(new Comment("User 7", "Fantastic video, Thank you!"));
+        _videos[2]._comments.Add(new Comment("User 8", "I subscribed after watching this."));
+        _videos[2]._comments.Add(new Comment("User 9", "This is my new favorite channel."));
+
+        // Display information for each video
+        foreach (var _video in _videos)
+        {
+            Console.WriteLine($"Title: {_video._title}");
+            Console.WriteLine($"Author: {_video._author}");
+            Console.WriteLine($"Length: {_video._length} seconds");
+            Console.WriteLine($"Number of comments: {_video.GetCommentCount()}");
+            Console.WriteLine("Comments: ");
+            foreach (var _comment in _video._comments)
+            {
+                Console.WriteLine($"- {_comment._commenter}: {_comment._text}");
+            }
+            Console.WriteLine();
+        }
     }
+
+    
 }
